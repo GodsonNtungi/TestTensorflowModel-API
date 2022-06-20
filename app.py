ec5ed4e6b2cc9ea09eb147131a782c6568916bce
GodsonNtungi <godsonezekiel31@gmail.com>
2022-06-20 23:21:58 +0300

    image api

--- app.py
+++ app.py
@@ -1,24 +1,33 @@
-from flask import Flask,jsonify,request
+import base64
+
+import io
+
+from PIL import Image
+from flask import Flask, jsonify, request
 import joblib
 import sklearn
+
 model = joblib.load('testmodel.pkl')
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return jsonify({'text':'helloworld'})
+    return jsonify({'text': 'helloworld'})
 
-@app.route('/testprediction',methods =['POST'])
+
+@app.route('/testprediction', methods=['POST'])
 def Picture_prediction():
     if request.method == 'POST':
         some_json = request.get_json()
-        value = model.predict([some_json['some json']])
-        return jsonify({'value':int(value[0])})
+        image_bytes=some_json['body'].encode()
+        value = base64.b64decode( image_bytes)
+        image = Image.open(io.BytesIO(value))
+        image.save('out883.jpg')
 
 
-
+        return jsonify({'value':1})
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=4000,debug=True)
